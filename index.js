cb281b1545c4d0f7f36b1662772a6063ec3899b1
Tanmoym047 <tanmoym047@gmail.com>
2026-01-02 21:04:07 +0600

    socket io cors fixed

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 
-        'https://sketchspace-46391.web.app', 
+    origin: ['http://localhost:5173',
+        'https://sketchspace-46391.web.app',
         'https://sketchspace-46391.firebaseapp.com',
         'https://sketchspace-server.onrender.com'
     ],
@@ -22,6 +22,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmbltxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -34,7 +36,11 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173",
+            'https://sketchspace-46391.web.app',
+            'https://sketchspace-46391.firebaseapp.com',
+            'https://sketchspace-server.onrender.com'
+        ],
         methods: ["GET", "POST"]
     }
 });
@@ -115,6 +121,7 @@ async function run() {
                 $set: {
                     name: name,
                     elements: elements,
+                    files: files || {},
                     lastModified: new Date()
                 },
                 // Set these ONLY if the document is being created for the first time
